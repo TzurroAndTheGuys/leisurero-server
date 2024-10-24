362c728152937e7ddbab02811e40fa1f10b3ac61
Hejow <gmlwh124@naver.com>
2024-10-24 23:01:18 +0900

    chore: add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,45 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-  kotlin("jvm") version "1.9.25"
-  kotlin("plugin.spring") version "1.9.25"
   id("org.springframework.boot") version "3.3.4"
   id("io.spring.dependency-management") version "1.1.6"
+  kotlin("jvm") version "1.9.25"
+  kotlin("plugin.spring") version "1.9.25"
   kotlin("plugin.jpa") version "1.9.25"
+  id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
+
+val jar: Jar by tasks
 
 group = "com"
 version = "0.0.1-SNAPSHOT"
 
-java {
-  toolchain {
-    languageVersion = JavaLanguageVersion.of(17)
-  }
-}
+jar.enabled = false
+java.toolchain.languageVersion = JavaLanguageVersion.of(17)
 
-repositories {
-  mavenCentral()
-}
+apply(from = "gradle/ktlint.gradle")
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.boot:spring-boot-starter-mail")
-  implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-  implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-mail")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  runtimeOnly("com.mysql:mysql-connector-j")
+
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+  implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-  runtimeOnly("com.mysql:mysql-connector-j")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-  testImplementation("org.springframework.security:spring-security-test")
-  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
   compilerOptions {
     freeCompilerArgs.addAll("-Xjsr305=strict")
+    jvmTarget = JvmTarget.JVM_17
   }
 }
 
@@ -49,6 +49,11 @@ allOpen {
   annotation("jakarta.persistence.Embeddable")
 }
 
-tasks.withType<Test> {
-  useJUnitPlatform()
+noArg {
+  annotation("jakarta.persistence.Entity")
+  annotation("jakarta.persistence.Embeddable")
+}
+
+repositories {
+  mavenCentral()
 }
